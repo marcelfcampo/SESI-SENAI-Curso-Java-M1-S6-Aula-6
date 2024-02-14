a6004ea2e129227a901834a89299a634dceefbb8
Marcel Ferreira Campos <marcelfcampos@gmail.com>
2024-02-14 17:11:17 -0300

    1-Banco de Dados SQL 2- SpringRestApiPostgre.iml
    1-Banco de Dados SQL
    2- SpringRestApiPostgre.iml

---
+++ Exercicio1Semana6/SpringRestApiPostgre/src/test/java/com/springbd/SpringRestApiPostgre/SpringRestApiPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbd.SpringRestApiPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
